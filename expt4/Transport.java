c30283c0a8f237a33f5f40bc03dbd3967b9747b2
Sanjeeth <111098807+Sanjeeth8002@users.noreply.github.com>
2022-09-13 17:48:04 +0530

    Update and rename transport to Transport.java

--- expt4/Transport.java
+++ expt4/Transport.java
@@ -1,24 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package EXP4;
-
 import java.util.Scanner;
-
-/**
- *
- * @author 21CSE079
- */
 public class transport
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     int n;
     System.out.println("ENTER NO OF OBJECTS-->\n");
     Scanner o=new Scanner(System.in);
