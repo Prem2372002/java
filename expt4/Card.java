bae54abda0589d8dd0fb3192b2f300266bcdb523
Sanjeeth <111098807+Sanjeeth8002@users.noreply.github.com>
2022-09-13 17:49:28 +0530

    Update and rename card to Card.java

--- expt4/Card.java
+++ expt4/Card.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication4;
 import java.util.*;
-/**
- *
- * @author USER
- */
 public class card {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner ob=new Scanner(System.in);
         System.out.println("enter number of customer:");
         int n=ob.nextInt();
