c899383f4ab5463615c30dfc1b3e3fedde83f2ad
Prem2372002 <111497696+Prem2372002@users.noreply.github.com>
2022-09-20 18:35:09 +0530

    Create package3.java

---
+++ expt 5/packages and subpackages/pack/package3.java
@@ -0,0 +1,14 @@
+package packageexe.pack3;
+public class Package3 {
+    public void Pattern(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
