5cf9d1a9acc1d4291009322fc726b4f83039c8da
Prem2372002 <111497696+Prem2372002@users.noreply.github.com>
2022-09-20 18:34:03 +0530

    Create package1.java

---
+++ expt 5/packages and subpackages/pack/package1.java
@@ -0,0 +1,10 @@
+package packageexe.pack1;
+public class Paackage1 {
+    public void calculation(int a, int b)
+    {
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction: "+(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.println("Division:" +(a/b));
+    }
+}
